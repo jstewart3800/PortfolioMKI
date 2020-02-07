e6fc7b56ac5c2df2dea5fca1b24840afe1b7bbdd
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-02-06 20:22:37 -0800

    Restarted, built template

--- portfolioSite/e2e/src/app.e2e-spec.ts
+++ portfolioSite/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Portfolio app is running!');
+    expect(page.getTitleText()).toEqual('portfolioSite app is running!');
   });
 
   afterEach(async () => {
--- portfolioSite/src/app/app.component.spec.ts
+++ portfolioSite/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Portfolio'`, () => {
+  it(`should have as title 'portfolioSite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Portfolio');
+    expect(app.title).toEqual('portfolioSite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Portfolio app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('portfolioSite app is running!');
   });
 });
